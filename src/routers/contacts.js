3dbb80a9d45809694265f2db806bb2408e4affbe
dfostakovskyi <d.fostakovskiy@gmail.com>
2025-03-30 16:35:40 -0700

    Fixing

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,51 +1,5 @@
 //src\routers\contacts.js
 
-// import { Router } from 'express';
-// import {
-//   getContactsController,
-//   getContactByIdController,
-//   postContactController,
-//   patchContactController,
-//   deleteContactController,
-// } from '../controllers/contacts.js';
-// import ctrlWrapper from '../utils/ctrlWrapper.js';
-// import { validateBody } from '../middlewares/validateBody.js';
-// import { isValidId } from '../middlewares/isValidId.js';
-// import { authenticate } from '../middlewares/authenticate.js';
-// import {
-//   createContactSchema,
-//   updateContactSchema,
-// } from '../validation/contacts.js';
-
-// const contactsRouter = Router();
-
-// contactsRouter.use(authenticate);
-
-// contactsRouter.get('/', ctrlWrapper(getContactsController));
-// contactsRouter.get(
-//   '/:contactId',
-//   isValidId,
-//   ctrlWrapper(getContactByIdController),
-// );
-// contactsRouter.post(
-//   '/',
-//   validateBody(createContactSchema),
-//   ctrlWrapper(postContactController),
-// );
-// contactsRouter.patch(
-//   '/:contactId',
-//   isValidId,
-//   validateBody(updateContactSchema),
-//   ctrlWrapper(patchContactController),
-// );
-// contactsRouter.delete(
-//   '/:contactId',
-//   isValidId,
-//   ctrlWrapper(deleteContactController),
-// );
-
-// export default contactsRouter;
-
 import { Router } from 'express';
 import {
   getContactsController,
@@ -75,7 +29,6 @@ contactsRouter.get(
   ctrlWrapper(getContactByIdController),
 );
 
-// Для створення контакту: обробка multipart/form-data за допомогою multer
 contactsRouter.post(
   '/',
   upload.single('photo'),
@@ -83,7 +36,6 @@ contactsRouter.post(
   ctrlWrapper(postContactController),
 );
 
-// Для оновлення контакту: обробка файлу та інших даних одночасно
 contactsRouter.patch(
   '/:contactId',
   isValidId,
