3b6d5bba92e952ba3050dc405c07b18927e348bf
dfostakovskyi <d.fostakovskiy@gmail.com>
2025-03-31 13:11:24 -0700

    Changed incorrect login and password

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,13 +1,13 @@
 // src\utils\sendMail.js
 
 import nodemailer from 'nodemailer';
-
 import { SMTP } from '../constants/index.js';
 import { getEnvVar } from '../utils/getEnvVar.js';
 
 const transporter = nodemailer.createTransport({
   host: getEnvVar(SMTP.SMTP_HOST),
   port: Number(getEnvVar(SMTP.SMTP_PORT)),
+  secure: false,
   auth: {
     user: getEnvVar(SMTP.SMTP_USER),
     pass: getEnvVar(SMTP.SMTP_PASSWORD),
@@ -15,5 +15,17 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendEmail = async (options) => {
-  return await transporter.sendMail(options);
+  const mailOptions = {
+    from: getEnvVar(SMTP.SMTP_FROM),
+    ...options,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log(`Email sent: ${info.response}`);
+    return info;
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error;
+  }
 };
